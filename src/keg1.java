ec670641eec9c95c335d52573cdbe6350184551b
anantapir <115287802+Ananta-Pirdhaus@users.noreply.github.com>
2023-04-13 18:03:02 +0700

    Initial commit

---
+++ src/keg1.java
@@ -0,0 +1,6 @@
+package PACKAGE_NAME;/**
+    * @created 13/04/2023 - 17:53
+    * @project latihan
+    * @author anant
+*/public class keg1 {
+}
---
+++ src/mainkeg2.java
@@ -0,0 +1,6 @@
+package PACKAGE_NAME;/**
+    * @created 13/04/2023 - 17:57
+    * @project latihan
+    * @author anant
+*/public class mainkeg2 {
+}
